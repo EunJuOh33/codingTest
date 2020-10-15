9307d804fa45649da7045a500a982c0ffd28edc3
dooeun <58769961+dooeun@users.noreply.github.com>
2020-10-15 18:11:34 +0900

    프로그래머스_Lv_1_완주하지 못한 선수_MashMap이용

--- src/programmersLv_1/player/Solution2.java
+++ src/programmersLv_1/player/Solution2.java
@@ -1,5 +1,6 @@
 package programmersLv_1.player;
 
+import java.util.HashMap;
 
 public class Solution2 {
 		/*
@@ -17,7 +18,17 @@ public class Solution2 {
 		
 		public String solution(String[] participant, String[] completion) {
 			String answer = "";
-				
+			HashMap<String, Integer> hm = new HashMap<>();
+			
+	        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+	        
+	        for (String player : completion) hm.put(player, hm.get(player) - 1);
+
+	        for (String key : hm.keySet()) {
+	            if (hm.get(key) != 0) {
+	                answer = key;
+	            }
+	        }
 			
 			return answer;
 		}
