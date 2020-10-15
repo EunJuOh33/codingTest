0c53ca365385c360c0fc20b9cbb22854d628b947
dooeun <58769961+dooeun@users.noreply.github.com>
2020-10-15 16:35:10 +0900

    프로그래머스_Lv_1_완주하지 못한 선수

---
+++ src/programmersLv_1/player/Solution.java
@@ -0,0 +1,46 @@
+package programmersLv_1.player;
+
+import java.util.Arrays;
+
+public class Solution {
+	/*
+	 * 한 명의 선수를 제외하고는 모든 선수가 마라톤 완주
+	 * 
+	 * - 선수의 수 : 1 <= num <= 100,000
+	 * - completion의 길이는 participant의 길이보다 1 작다.
+	 * - 참가자의 이름은 1 <= name <= 20 알파벳 소문자
+	 * - 참가자 중 동명이인이 있을 수 있다.
+	 * 
+	 * participant : 마라톤에 참여한 선수들의 이름이 담긴 배열
+	 * completion  : 마라톤을 완주한 선수들의 이름이 담긴 배열
+	 * return : 완주하지 못한 선수의 이름 
+	 */
+	
+	public String solution(String[] participant, String[] completion) {
+		String answer = "";
+		// 정렬을 하는 이유 : participant[i]와 completion[i]를 비교해야 하기 때문 
+		Arrays.sort(participant);	// [ "mislav", "stanko", "mislav", "ana" ]
+		Arrays.sort(completion);	// [ "stanko", "mislav", "ana" ]	-> 동명이인인 "mislav"가 완주하지 못한 선수.
+		
+		int i;
+		for(i=0; i < completion.length; i++) {
+			if(!participant[i].equals(completion[i])) {
+				return participant[i];	
+				// return은 실행 중인 구문을 중단시키고 바로 값을 return 시킨다.
+				// 참가자 배열과 완주자 배열에 다른 이름이 하나라도 있으니(완주하지 못한 선수) 그 이름을 리턴시킨다.
+			}
+		}
+		
+		return participant[i];
+	}
+	
+	
+	/* 테스트 - 실행 */
+	public static void main(String[] args) {
+		String[] participant = {"mislav", "stanko", "mislav", "ana"};
+		String[] completion = {"stanko", "mislav", "ana"};
+		
+		Solution sol = new Solution();
+		System.out.println(sol.solution(participant, completion));
+	}
+}
